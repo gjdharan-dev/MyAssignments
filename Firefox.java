d910921c244c0bf257a3be5c5089b1c349aba7f2
gjdharan-dev <gjdharan@gmail.com>
2026-03-01 08:24:15 +0530

    Add files via upload
    Day 1 Assignments Uploaded

---
+++ Firefox.java
@@ -0,0 +1,16 @@
+package org.amazon.week1.day1;
+
+public class Firefox {
+
+    public static void main(String[] args) {
+        float browserVersion=100.2f;
+        String browserName="firefox";
+        boolean isVisible=true;
+        int releaseyear=1998;
+        char browserlogo='f';
+        System.out.println(browserVersion+" "+browserName+" "+isVisible+" "+releaseyear+" "+browserlogo);
+
+        System.out.println("");
+
+    }
+}
