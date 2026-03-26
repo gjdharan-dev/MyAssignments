b91d2d3ddbad9503ae34d2dfab10a20fd0507ce7
Vidhyaa Dharan gj <gjdharan@gmail.com>
2026-03-26 15:53:19 +0530

    Add files via upload

---
+++ Week4Day1Assignments/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package org.amazon.week4Assignmesnts;
+
+public interface DatabaseConnection {
+        
+
+    public void connect();
+
+    public void disconnect();
+
+    public void executeUpdate();
+
+    
+
+}
